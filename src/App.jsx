13a90d472280a6e4f925ba55a7d77a4f9ef98303
sabrinaahamed <sabrinaahamed.official@gmail.com>
2026-03-05 14:46:56 +0600

    Increment function

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { use } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { increment } from './slices/counterSlice'
 
 const App = () => {
+  let dispatch = useDispatch()
+  let data=useSelector((state)=>state.counter.value)  
   return (
-    <div>
-       <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
+    <div className='h-screen w-screen flex flex-col items-center justify-center gap-5'>
+      <h1 className='text-2xl'>Counter {data}</h1>
+      <button onClick={()=> dispatch(increment(1))} className='px-[20px] py-[10px] bg-black text-white rounded-2xl'>Increment</button>
+  
     </div>
   )
 }
--- src/slices/counterSlice.js
+++ src/slices/counterSlice.js
@@ -1,29 +1,22 @@
 import { createSlice } from '@reduxjs/toolkit'
 
 export const counterSlice = createSlice({
-  name: 'counter',
+  name: 'count',
   initialState: {
     value: 0,
   },
   reducers: {
-    increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes.
-      // Also, no return statement is required from these functions.
-      state.value += 1
+    increment: (state,action) => {
+    state.value +=action.payload
     },
-    decrement: (state) => {
-      state.value -= 1
+    decrement: (state,action) => {
+      state.value -= action.payload
     },
-    incrementByAmount: (state, action) => {
-      state.value += action.payload
-    },
+  
   },
 })
 
 // Action creators are generated for each case reducer function
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const { increment, decrement, } = counterSlice.actions
 
 export default counterSlice.reducer
